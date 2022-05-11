c0143a22a964239c0cd1c56b9787c688c061a379
Zeynep HAN <zeynephan9906@gmail.com>
2022-05-11 16:46:02 +0300

    11.05.2022

--- src/lambda_practice/C3_MultiArrays_StreamOrnekleri.java
+++ src/lambda_practice/C3_MultiArrays_StreamOrnekleri.java
@@ -1,10 +1,11 @@
 package lambda_practice;
-//multi arrayleri cevirirken flapMap kullaniriz yapilis sekli iki turludur
-//1) lamda kullanarak
-// Arrays.stream(arr).flatMap(t->Arrays.stream(t))
-//2) method ile
-// Arrays.stream(arr).flatMap(Arrays::stream)
-// BU SEKLIDE ARTIK TUM STREAM METHODLARI KULLANILABILIR
+
+            //multi arrayleri cevirirken flapMap kullaniriz yapilis sekli iki turludur
+            //1) lamda kullanarak
+            // Arrays.stream(arr).flatMap(t->Arrays.stream(t))
+            //2) method ile
+            // Arrays.stream(arr).flatMap(Arrays::stream)
+            // BU SEKLIDE ARTIK TUM STREAM METHODLARI KULLANILABILIR
 
 import java.util.Arrays;
 import java.util.List;
@@ -15,34 +16,47 @@ public class C3_MultiArrays_StreamOrnekleri {
         String arr[][] = {
                 {"Elma", "Muz"},
                 {"Portakal", "Cilek", "Limon"},
-                {"Havuc","Erik"}
+                {"Havuc", "Erik"}
         };
 
-        System.out.println(liste(arr));
+        System.out.println(listeleme(arr));
+        ciftYazdirma(arr);
+        System.out.println("EListe(arr) = " + EListe(arr));
+        kIleBiten(arr);
 
 
     }
+
     // S1 : tum elemanlari list yapalim
-    public static List<String> liste(String[][] arr){
+    // flapmap cift boyuttan cikarmak icin kullaniliyor
+    public static List<String> listeleme(String[][] arr) {
+        return Arrays.stream(arr).flatMap(t -> Arrays.stream(t)).collect(Collectors.toList());
 
-
-        return null;
     }
-
-
-
-
 
 
     // S2: E ile baslayan elemanlari double (elmaelma) olarak yazdiralim
 
+    public static void ciftYazdirma(String[][] arr) {
+        Arrays.stream(arr).flatMap(t -> Arrays.stream(t)).filter(t -> t.startsWith("E")).map(t -> t + t).forEach(t -> System.out.println(t));
 
+    }
 
     // S3: E ile baslayan elemanlari liste olarak yazdiralim
+    public static List<String> EListe(String[][] arr) {
+        return Arrays.stream(arr).
+                flatMap(t -> Arrays.stream(t)).
+                filter(t -> t.toLowerCase().startsWith("e")).
+                collect(Collectors.toList());
 
+    }
 
 
     //S4 : k ile bitenlerin sonuna '*' ekleyelim
+    public static void kIleBiten(String[][] arr) {
+              Arrays.stream(arr).
+                flatMap(t -> Arrays.stream(t)).
+                filter(t -> t.endsWith("k")).map(t -> t + "*").forEach(t-> System.out.print(t+ " "));
 
-
+    }
 }
--- src/lambda_practice/C6_Lambdada_ForLoop_Aralik_Kontrolu.java
+++ src/lambda_practice/C6_Lambdada_ForLoop_Aralik_Kontrolu.java
@@ -1,50 +1,113 @@
 package lambda_practice;
 
+import java.util.OptionalDouble;
 import java.util.stream.IntStream;
 
 public class C6_Lambdada_ForLoop_Aralik_Kontrolu {
 
     public static void main(String[] args) {
 
-
+        ilk30(30);
+        System.out.println("");
+        ilk30Dahil(30);
+        System.out.println("");
+        System.out.println("top(10,40) = " + top(10, 40));
+        System.out.println("");
+        System.out.println("ortalama(20,30) = " + ortalama(20, 30));
+        System.out.println("");
+        System.out.println("sekizeBolunen(20,30) = " + sekizeBolunen(20, 30));
+        System.out.println("");
+        sekizeBolunenYaz(325, 468);
+        System.out.println("");
+        System.out.println("sekizeBolunenTopYaz(325,468) = " + sekizeBolunenTopYaz(325, 468));
+        System.out.println("");
+        System.out.println("tekSayiCrpm(7, 15) = " + tekSayiCrpm(7, 15));
+        System.out.println("");
+        ilk10Tek(10);
+        System.out.println("");
+        yirmiYediveYedininKati(10);
+        System.out.println("");
+        System.out.println("yirmiYediveYedininKatiTopla(20) = " + yirmiYediveYedininKatiTopla(20));
     }
 
     // S1:1 den 30 kadar olan sayilari (30 dahil degil) 1 2 3 .... seklinde siralayalim (for loopsuz)
     //range(int startInclusive, int endExclusive)
 
+    public static void ilk30(int a) {
+        IntStream.range(1, a).forEach(t -> System.out.print(t + " "));
+    }
 
     //S2 1 den 30 kadar olan sayilari (30 dahil ) 1 2 3 .... seklinde siralayalim (for loopsuz)
     //rangeClosed(int startInclusive, int endInclusive)
+    public static void ilk30Dahil(int a) {
+        IntStream.rangeClosed(1, a).forEach(t -> System.out.print(t + " "));// a+1 de oluyor
+    }
 
 
-    //S3 Istenen iki deger(dahi) arasindaki sayilari toplayalim
+    //S3 Istenen iki deger(dahil) arasindaki sayilari toplayalim
+
+    public static int top(int bas, int bitis) {
+        return IntStream.rangeClosed(bas, bitis).sum();
+    }
 
 
     //S4: 30 ile 40 arasindaki sayilarin (dahi) ortalamasini bulalim
 
-
+    public static OptionalDouble ortalama(int bas, int bitis) {
+        return IntStream.rangeClosed(bas, bitis).average(); // ortalama alir
+    }
     //S5: 325 ile 468 arasinda 8 e bolunen kac sayi vardir?
+
+    public static long sekizeBolunen(int bas, int bitis) {
+
+        return IntStream.rangeClosed(bas, bitis).filter(t -> t % 8 == 0).count();
+    }
 
 
     //S6: 325 ile 468 arasinda 8 e bolunen sayilari yazdiralim
+    public static void sekizeBolunenYaz(int bas, int bitis) {
+
+        IntStream.rangeClosed(bas, bitis).filter(t -> t % 8 == 0).forEach(t -> System.out.print(t + " "));
+    }
 
 
     // S7:325 ile 468 arasinda 8 e bolunen sayilarin toplamini bulalim
+    public static int sekizeBolunenTopYaz(int bas, int bitis) {
+
+        return IntStream.rangeClosed(bas, bitis).filter(t -> t % 8 == 0).sum();
+    }
 
 
     // S8: 7ile 15 arasindaki tek sayilarin carpimini bulalim
 
+    public static double tekSayiCrpm(int bas, int bitis) {
+        return IntStream.rangeClosed(bas, bitis).filter(Utils::printOddElements).reduce(1, (x, y) -> x * y);
+     // reduce ilk buldugu 7'yi alir 9la carpar sonrasinda sonucu 11 le carpar ve sirayla ilerler
+    }
 
     //S9: pozitif tek sayilarin ilk 10 elemanin yazdiralim
+
+    public static void ilk10Tek(int a ){
+       IntStream.iterate(1,t->t+2).limit(10).forEach(t-> System.out.print(t+" "));
+       // IntStream.rangeClosed(1,a).limit(10).filter(Methods::tekMi).forEach(Methods::yazInteger); 1 ile 10 arasi tek sayilari verir
+       //limit 10 cunku 10 tane istiyor eger kullanicidan girmesini isteseydik a olmaliydi
+    }
 
 
     //S10: 21 den baslayan 7 nin katlarinin tek olanlari ilk 10 teriminin yaziralim
 
+    public static void yirmiYediveYedininKati(int a ){
+        IntStream.iterate(21,t->t+7).limit(10).forEach(Methods::yazInteger);
+    }
 
 
     //S11: 21 den baslayan 7 nin katlarinin ilk 20 teriminin toplayalim
+    public static int yirmiYediveYedininKatiTopla(int a ){
 
-}
+        return IntStream.iterate(21,t->t+7).limit(20).sum();
+    }
+
 //iterate bize sayilari istedigimiz sekilde yineletmeyi saglar yani buradaki ornekte 7ser7ser artmayi saglar
 //iterate icin bir baslangic degeri girilmeli ve artisin nasil olacagi belirtilmeli nerede biteceginide
 // limit() ile belirliyoruz
+}
